d6c51c725a05960c015488b01a31bafc7b42ea73
reemagt2018 <reemagt2018@gmail.com>
2018-09-08 20:40:23 -0400

    new dir

--- Paragraph.py
+++ Paragraph.py
@@ -9,13 +9,14 @@ def splitParagraphIntoSentences(paragraph):
 
     sentenceEnders = re.compile('[.!?]')
     sentenceList = sentenceEnders.split(paragraph)
-    print  (sentenceList)
+    #print  (sentenceList)
     return sentenceList
 
-paragraph=input("Enter Paragraph")
+#paragraph=input("Enter Paragraph")
+paragraph="Life is one huge emotional mind game. There are days we wake up with an empty heart wondering if we’re even going to make it. We get beaten to the ground over and over again, we get our hope torn from us, we scream at the top of our lungs to the sky hoping that the moon will fill the void that still remains.. But somehow through all the pain, through all the suffering.. These moments never compare to the seconds that take our breath away. The situations given to us that remind us of how beautiful life can be. Let go.. Detach yourself.. There’s too much beauty to quit. I live for moments like these… WE live for moments like these."
 
 sentences = splitParagraphIntoSentences(paragraph)
-print (sentences)
+#print (sentences)
 numberofsentences=len(list(sentences))
 print (numberofsentences)
 char=0
@@ -26,13 +27,26 @@ for s in sentences:
       char=char+1
       if(i==' '):
             word=word+1
-print("Number of words in the paragraph:")
-print(word)
-print("Number of characters in the paragraph:")
-print(char)
-print ("Number of sentences")
-print(numberofsentences)
-print ("Average Letter Count")
-print (char/word)
-print ("Average Sentence Length")
-print (word/numberofsentences)
+
+print (" Paragraph Analysis ")
+print ("-------------------")
+print(f'Approximate Word Count :  {word} ')
+#print(word)
+# print("Number of characters in the paragraph:")
+# print(char)
+print (f'Approximate Sentence Count : {numberofsentences}')
+#print(numberofsentences)
+print (f'Average Letter Count : {char/word}' )
+#print (char/word)
+print (f'Average Sentence Length : {word/numberofsentences} ')
+#print (word/numberofsentences)
+
+
+
+#
+# Paragraph Analysis
+# -----------------
+# Approximate Word Count: 122
+# Approximate Sentence Count: 5
+# Average Letter Count: 4.6
+# Average Sentence Length: 24.0
